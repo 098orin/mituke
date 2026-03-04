df8d09550b9abfacd06363640f268cd1e119c88c
098orin <098orin0102@gmail.com>
2026-03-04 18:40:23 +0900

    VC に入室させるコマンドを作成

--- main.py
+++ main.py
@@ -15,6 +15,24 @@ intents.voice_states = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 
+@bot.command()
+async def join(ctx: commands.Context, arg):
+    # Guild 内で使用されていることを保証する
+    if not isinstance(ctx.author, discord.Member):
+        await ctx.send("サーバー内でのみ使用できます")
+        return
+
+    voice_state = ctx.author.voice
+    if voice_state is None or voice_state.channel is None:
+        await ctx.send("ボイスチャンネルに参加してからコマンドを使用してください")
+        return
+
+    channel = voice_state.channel
+
+    await channel.connect()
+    await ctx.send(f"Joined {channel.name}!")
+
+
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
